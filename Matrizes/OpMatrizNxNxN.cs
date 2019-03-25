d91b59ecc58be4731cf8bb5c8943e97682e2e49f
Laboratório 06 <usrlab06@lanfran.local>
2019-03-25 11:16:54 -0300

    Implementado multiplicação NXNXN

--- Matrizes/OpMatrizNxNxN.cs
+++ Matrizes/OpMatrizNxNxN.cs
@@ -6,39 +6,48 @@ namespace Matrizes
 {
     public class OpMatrizNxNxN
     {
-        public int[,,] MultMatrizNxNxN(int[,,] matrizA, int[,,] matrizB)
+        public int[,,,] MultMatrizNxNxN(int[,,] matrizA, int[,,] matrizB)
         {
-            int sizeMatix = matrizA.GetLength(0);
-            int[,,] matrizResult = new int[sizeMatix, sizeMatix, sizeMatix];
+            long size = matrizA.GetLongLength(0);
+            int[,,,] matrizResult = new int[size, size, size, size];
             int aux;
 
-            //variação eixo Z
-            for (int z = 0; z < matrizA.GetLength(2);)
+            for (int u = 0; u <size; u++)
             {
-                //Variação eixo Y == coluna
-                for (int y = 0; y < matrizA.GetLength(1); y++)
+                for (int z = 0; z < size; z++)
                 {
-                    //Variação eixo X == linha
-                    for (int x = 0; x < matrizA.GetLength(0); x++)
+                    for (int l = 0; l < size; l++)
                     {
-                        aux = 0;
-                        for (int k = 0; k < matrizA.GetLength(0); k++)
+                        //Variação eixo Y == coluna
+                        for (int y = 0; y < size; y++)
                         {
-                            aux += matrizA[z,y, k] * matrizB[z,k, x];
+                            //Variação eixo X == linha
+                            for (int x = 0; x < size; x++)
+                            {
+                                aux = 0;
+                                for (int k = 0; k < size; k++)
+                                {
+                                    aux += matrizA[z, y, k] * matrizB[l, k, x];
+                                }
+
+                                matrizResult[u, z, y, x] = aux;
+
+
+                            }
                         }
-
-                        matrizResult[z, y, x] = aux;
-
-
                     }
                 }
 
-             
+
+
+
             }
+
+
             return matrizResult;
         }
 
-            
     }
 }
-}
+   
+
---
+++ TesterOpMatriz/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Matrizes;
+
+namespace TesterOpMatriz
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            OpMatrizNxNxN op = new OpMatrizNxNxN();
+            int[,,] vec = { { { 2, 4}, { 3, 0 } },
+                            { {2, 4}, { 5, 6} } };
+
+
+
+            int[,,] vec2 = { { { 2, 3 }, { 3, 4 } },
+                             { {2, 4}, { 5, 6} } };
+
+
+            int[,,,] resposta = op.MultMatrizNxNxN(vec, vec2);
+
+
+
+            for (int u = 0; u < resposta.GetLength(0); u++)
+            {
+                Console.Write("\n\n[CUBO {0}]", u);
+                for (int z = 0; z < resposta.GetLength(0); z++)
+                {
+                    Console.Write("\n[TABELA{0}]", z);
+                    //mostra vetor
+                    for (int i = 0; i < resposta.GetLength(0); i++)
+                    {
+                        Console.WriteLine();
+                        for (int j = 0; j < resposta.GetLength(1); j++)
+                            Console.Write("[{0}]", resposta[u, z, i, j]);
+                    }
+                }
+            }
+
+            
+            Console.WriteLine();
+
+        }
+    }
+}
